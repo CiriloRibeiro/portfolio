18ce21565927c61b1ca7d1500fb8da7dda8c704a
CiriloRibeiro <cirilo.ribeiroluiz@gmail.com>
2024-04-28 12:45:33 -0300

    r13

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,6 @@ echo "Building the project..."
 pip3 install -r requirements.txt
 
 echo "Make migration and collect static..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput --clear
