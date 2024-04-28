ff1275b3c0e60a912b6f0047d6c57c4f791ea0db
CiriloRibeiro <cirilo.ribeiroluiz@gmail.com>
2024-04-28 20:10:39 -0300

    portfolio complete

--- projects/admin.py
+++ projects/admin.py
@@ -4,23 +4,23 @@ from .models import Post, Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('headline', 'created_at', 'has_image')
+    list_display = ('headline', 'created_at')
     search_fields = ('headline', 'content')
     filter_horizontal = ('categories',)  # Add this line if you want a horizontal filter for categories
-    readonly_fields = ('image_preview',)
+    # readonly_fields = ('image_preview',)
 
-    def has_image(self, obj):
-        return bool(obj.image)
-    has_image.boolean = True
-    has_image.short_description = 'Has Image'
+    # def has_image(self, obj):
+    #     return bool(obj.image)
+    # has_image.boolean = True
+    # has_image.short_description = 'Has Image'
 
-    def image_preview(self, obj):
-        if obj.image:
-            return format_html('<a href="{0}" target="_blank"><img src="{0}" style="max-height:200px;max-width:200px;" /></a>', obj.image.url)
-        else:
-            return 'No image'
-    image_preview.allow_tags = True
-    image_preview.short_description = 'Image Preview'
+    # def image_preview(self, obj):
+    #     if obj.image:
+    #         return format_html('<a href="{0}" target="_blank"><img src="{0}" style="max-height:200px;max-width:200px;" /></a>', obj.image.url)
+    #     else:
+    #         return 'No image'
+    # image_preview.allow_tags = True
+    # image_preview.short_description = 'Image Preview'
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- projects/models.py
+++ projects/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     created_at = models.DateField(auto_now_add=True)
     headline = models.TextField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    image_link = models.URLField(blank=True)
     project_link = models.URLField(blank=True)
     categories = models.ManyToManyField(Category)
 
